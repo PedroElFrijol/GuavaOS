edb559ad01de0177fd980fa7ed461a9d1f680bd5
MJ <munchkinrios@gmail.com>
2021-07-08 16:35:40 -0700

    added gnu-efi

--- kernel/run.bat
+++ kernel/run.bat
@@ -1,6 +1,6 @@
 set OSNAME = GuavaOS
-set BUILDFolder = bin
+set BUILDDIR = bin
 set OpenVirtualMachineFirmware = ../OVMFbin
 
-qemu-system-x86_64 -drive file=%BUILDFolder%/%OSNAME%.img -m 256M -cpu qemu64 -drive if=pflash,format=raw,unit=0,file="%OpenVirtualMachineFirmware%/OVMF_CODE-pure-efi.fd",readonly=on -drive if=pflash,format=raw,unit=1,file="%OVMFDIR%/OVMF_VARS-pure-efi.fd" -net none
+qemu-system-x86_64 -drive file=%BUILDDIR%/%OSNAME%.img -m 256M -cpu qemu64 -drive if=pflash,format=raw,unit=0,file="%OpenVirtualMachineFirmware%/OVMF_CODE-pure-efi.fd",readonly=on -drive if=pflash,format=raw,unit=1,file="%OVMFDIR%/OVMF_VARS-pure-efi.fd" -net none
 pause
