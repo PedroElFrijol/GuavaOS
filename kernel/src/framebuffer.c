73c8900d07aabe923e75f5d59ab11ef64c531f24
Marcelo Rios <45809332+PedroElFrijol@users.noreply.github.com>
2021-08-24 23:50:30 -0700

    Update framebuffer.c

--- kernel/src/framebuffer.c
+++ kernel/src/framebuffer.c
@@ -5,5 +5,5 @@ struct {
     int height;
     int bpp; //bits per pixel
     uint64_t fb; //unsigned integer 64 bits
-} KernelData = {0};
+} KernelData = {0}; //sets everything in the struct to null
 kernel_init(&KernelData);
