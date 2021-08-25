63a02f0fd19d20c24da7a6c1555d3accd2163d45
Marcelo Rios <45809332+PedroElFrijol@users.noreply.github.com>
2021-08-24 23:48:09 -0700

    Create framebuffer.c

---
+++ kernel/src/framebuffer.c
@@ -0,0 +1,9 @@
+include <stdint.h>
+
+struct {
+    int width;
+    int height;
+    int bpp; //bits per pixel
+    uint64_t fb; //unsigned integer 64 bits
+} KernelData = {0};
+kernel_init(&KernelData);
