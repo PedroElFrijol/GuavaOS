9e0e6a274dbdcf4677fd02e0ef9104502bcbc3e9
Marcelo Rios <45809332+PedroElFrijol@users.noreply.github.com>
2021-08-25 20:05:38 -0700

    Delete framebuffer.c

--- kernel/src/framebuffer.c
+++
@@ -1,9 +0,0 @@
-include <stdint.h>
-
-struct {
-    int width;
-    int height;
-    int bpp; //bits per pixel
-    uint64_t fb; //unsigned integer 64 bits
-} KernelData = {0}; //sets everything in the struct to null
-KernelInitalize(&KernelData);
