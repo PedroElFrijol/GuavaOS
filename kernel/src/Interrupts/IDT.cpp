fb9ce488ea22292d90f64194a531a46bda6eafda
Marcelo Rios <45809332+PedroElFrijol@users.noreply.github.com>
2021-10-12 00:15:56 -0700

    Update IDT.cpp

--- kernel/src/Interrupts/IDT.cpp
+++ kernel/src/Interrupts/IDT.cpp
@@ -1,8 +1,27 @@
 #include "IDT.h"
 #include <stdint.h>
 
-int main()
+void idt_set_gate(char vector, void *isr, char flags)
 {
-IDTR idtr;
-asm("lidtq %0" :: "m"(idtr));
+ struct idt_entry *descriptor = &idt[vector];
+
+ descriptor->base_lo = (unsigned int)isr & 0xFFFF;
+ descriptor->sel = 0x08; // this value can be whatever offset your kernel code selector is in your GDT
+ descriptor->flags = flags;
+ descriptor->base_hi = (unsigned int)isr >> 16;
+ descriptor->always0 = 0;
+}
+/* Installs the IDT */
+void idt_install()
+{
+ /* Sets the special IDT pointer up, just like in 'gdt.c' */
+ idtp.limit = (sizeof(struct idt_entry) * 256) - 1;
+ idtp.base = &idt;
+
+ /* Clear out the entire IDT, initializing it to zeros */
+ move_mem(&idt, 0, sizeof(struct idt_entry) * 256);
+
+ /* Add any new ISRs to the IDT here using idt_set_gate */
+ asm("lidtq %0" :: "m"(idtp));
+ /* Points the processor's internal register to the new IDT */
 }
