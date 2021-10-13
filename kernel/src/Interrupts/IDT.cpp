4e8bdf1dfd95d65ed70924d5e027fa7859994235
MJ <munchkinrios@gmail.com>
2021-10-12 17:24:18 -0700

    updated IDT

--- kernel/src/Interrupts/IDT.cpp
+++ kernel/src/Interrupts/IDT.cpp
@@ -3,23 +3,33 @@
 
 void idt_set_gate(char vector, void *isr, char flags)
 {
- struct idt_entry *descriptor = &idt[vector];
+ struct IDTEntry *descriptor = &idt[vector];
 
- descriptor->base_lo = (unsigned int)isr & 0xFFFF;
+ descriptor->isr_low = (unsigned int)isr & 0xFFFF;
  descriptor->sel = 0x08; // this value can be whatever offset your kernel code selector is in your GDT
  descriptor->flags = flags;
- descriptor->base_hi = (unsigned int)isr >> 16;
- descriptor->always0 = 0;
+ descriptor->isr_high = (unsigned int)isr >> 16;
+ descriptor->zero = 0;
 }
 /* Installs the IDT */
+
+void *move_mem(void *ptr, int value, int num)
+{
+ unsigned char *ptr_byte = (unsigned char *)ptr;
+
+ for (int i = 0; i < num; ptr_byte[i] = (unsigned char)value, i++)
+  ;
+ return ptr;
+}
+
 void idt_install()
 {
  /* Sets the special IDT pointer up, just like in 'gdt.c' */
- idtp.limit = (sizeof(struct idt_entry) * 256) - 1;
- idtp.base = &idt;
+ idtp.limit = (sizeof(struct IDTEntry) * 256) - 1;
+ idtp.base = (int)&idt;
 
  /* Clear out the entire IDT, initializing it to zeros */
- move_mem(&idt, 0, sizeof(struct idt_entry) * 256);
+ move_mem(&idt, 0, sizeof(struct IDTEntry) * 256);
 
  /* Add any new ISRs to the IDT here using idt_set_gate */
  asm("lidtq %0" :: "m"(idtp));
--- kernel/src/Interrupts/IDT.h
+++ kernel/src/Interrupts/IDT.h
@@ -2,13 +2,11 @@
 #include <stdint.h>
 
 typedef struct IDTEntry{
-	uint16_t    isr_low;      // The lower 16 bits of the ISR's address
-	uint16_t    kernel_cs;    // The GDT segment selector that the CPU will load into CS before calling the ISR
-	uint8_t	    ist;          // The IST in the TSS that the CPU will load into RSP; set to zero for now
-	uint8_t     attributes;   // Type and attributes; see the IDT page
-	uint16_t    isr_mid;      // The higher 16 bits of the lower 32 bits of the ISR's address
-	uint32_t    isr_high;     // The higher 32 bits of the ISR's address
-	uint32_t    reserved;     // Set to zero
+	unsigned short isr_low; // The lower 16 bits of the ISR's address
+	unsigned short sel;    // Our kernel segment goes here!
+	unsigned char zero; // this will be set to zero
+	unsigned char flags;   // Set using the above table
+	unsigned short isr_high; // The higher 16 bits of the ISR's address
 };
 
 //Struct packing
