674aa0c014ead5ba2907336f439a81cfce85106f
Marcelo Rios <45809332+PedroElFrijol@users.noreply.github.com>
2021-10-17 18:22:13 -0700

    Update run.bat

--- kernel/run.bat
+++ kernel/run.bat
@@ -1,5 +1,5 @@
 set OSNAME = GuavaOS
-set BUILDDIR = bin
+set BUILDDIR = build
 set OpenVirtualMachineFirmware = ../OVMFbin
 
 qemu-system-x86_64 -drive file=%BUILDDIR%/%OSNAME%.img -m 256M -cpu qemu64 -drive if=pflash,format=raw,unit=0,file="%OpenVirtualMachineFirmware%/OVMF_CODE-pure-efi.fd",readonly=on -drive if=pflash,format=raw,unit=1,file="%OVMFDIR%/OVMF_VARS-pure-efi.fd" -net none
