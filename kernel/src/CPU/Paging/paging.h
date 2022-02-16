09f5843a277fa148a745d34f73e0d69f522434eb
MJ <munchkinrios@gmail.com>
2022-02-15 20:18:38 -0700

    paging update

--- kernel/src/CPU/Paging/paging.h
+++ kernel/src/CPU/Paging/paging.h
@@ -1,7 +1,7 @@
 #include <stdint.h>
 
-extern "C" uint64_t pageDir[1024] __attribute__((aligned(4096)));
+extern uint64_t pageDir[1024] __attribute__((aligned(4096)));
 
-extern "C" uint64_t PageTable1[1024] __attribute__((aligned(4096))); //the first page table
+extern uint64_t PageTable1[1024] __attribute__((aligned(4096))); //the first page table
 
-extern "C" void PageDirectory();
+extern void PageDirectory();
