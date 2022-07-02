b35ddcb9aeb38abb41fe63c8e11056fd359df854
Marcelo Rios <marceloraulriosjr@gmail.com>
2022-07-01 18:15:15 -0700

    fixed errors

--- kernel/src/CPU/GDT/gdt.c
+++ kernel/src/CPU/GDT/gdt.c
@@ -3,7 +3,7 @@
 // sets up a descriptor in the gdt
 void gdt_set_gate(int num, unsigned long base, unsigned long limit, unsigned char access, unsigned char gran)
 {
-    // setsvp the descriptors base address
+    // sets up the descriptors base address
     gdt[num].base_low = (base & 0xFFFF);
     gdt[num].base_middle = (base >> 16) & 0xFF;
     gdt[num].base_high = (base >> 24) & 0xFF;
@@ -12,14 +12,14 @@ void gdt_set_gate(int num, unsigned long base, unsigned long limit, unsigned cha
     gdt[num].limit_low = (limit & 0xFFFF);
     gdt[num].granularity = ((limit >> 16) & 0x0F);
 
-    /* Finally, set up the granularity and access flags */
+    // sets up the granularity and access flags
     gdt[num].granularity |= (gran & 0xF0); // determines if the unit is a byte or a page
     gdt[num].access = access;
 }
 
 void install_gdt()
 {
-    // setsup the GDT pointer and limit
+    // sets up the GDT pointer and limit
     gp.limit = (sizeof(struct GDTEntry) * 3) - 1;
     gp.base = &gdt; // the address of gdt
 
